501cfd21aeecae190a1d1d5d2bbdf7d9c6781155
Martin Wyatt <martin.wyatt@gmail.com>
2018-02-04 21:31:49 +0000

    croppa added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,14 +3,20 @@
 <head>
     <title>My Application</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- 
+         _   _  _ _ _ 
+        | \_/ || | | |
+        | \_/ || V V |
+        |_| |_| \_n_/ 
+        Welcome fellow developers!
+    -->
 </head>
 <body>
-<div class="container">
-    
-@yield('content')
+    <div class="container">
+        
+        @yield('content')
 
-</div>
-<script type="text/javascript" src="{{URL::asset('js/app.js')}}"></script>
+    </div>
+    <script type="text/javascript" src="{{URL::asset('js/app.js')}}"></script>
 </body>
 </html>
- 
